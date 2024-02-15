d711a153f36bce8aa934dfd5591887d572da27f9
JvWandermurem <159195993+JvWandermurem@users.noreply.github.com>
2024-02-15 13:49:33 -0300

    Initial commit

---
+++ atividade-Nota-060224.js
@@ -0,0 +1,19 @@
+var nota1 = 5
+var nota2 = 2
+var notaf = (nota1 + nota2)/2
+console.log('sua nota foi',notaf);
+//condições "if" para colocar uma consição. 
+if(notaf > 7){
+    console.log('Párabens você foi aprovado =)');
+
+    
+}
+
+if(notaf = 7){
+   console.log( 'infelismente você foi reprovado =(')
+    
+}
+
+if(notaf < 4){
+    console.log('que feio! nota baixa, vai de suspensão meu querido ;O')
+}
---
+++ tarefa-while-e-for.js
@@ -0,0 +1,8 @@
+var i = 1;
+while( i <= 50) {
+    console.log(i)
+    i++;
+    
+}
+
+
